748d66ccf204a1b75e647658a232f585b3196b51
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-07-07 12:50:12 -0500

    Gulp-Tarea para js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
-// const concat = require('gulp-concat');
+const concat = require('gulp-concat');
+const browserify = require('gulp-browserify');
+const rename = require('gulp-rename');
+const addsrc = require('gulp-add-src');
+// const browserSync = require('browser-sync').create();
+
 
 const config = {
     source: './src/',
@@ -10,22 +15,26 @@ const config = {
 const paths = {
   html    : "**/*.html",
   assets  : "assets/",
-  // js      : "js/**/*.js",
+  js      : "js/**/*.js",
+  mainJS  : "js/app.js",
+  jquery  : "js/vendor/jquery-3.2.1.min.js",
+  jsMaterialize  : "js/vendor/materialize.min.js",
   sass    : "scss/**/*.scss",
   mainSass: "scss/main.scss"
-  // jquery  : "js/vendor/jquery-3.2.1.min.js",
-  // jsMaterialize  : "js/vendor/materialize.min.js",
-  // mainJS  : "js/app.js"
 };
 
 const sources = {
   html  : config.source + paths.html,
   assets: config.source + paths.assets,
+  js    : config.source + paths.assets + paths.js,
+  rootJS: config.source + paths.assets + paths.mainJS,
+  jquery: config.source + paths.assets + paths.jquery,
+  materialize: config.source + paths.assets + paths.jsMaterialize,
   sass  : config.source + paths.assets + paths.sass,
   rootSass: config.source + paths.assets + paths.mainSass
 };
 
-gulp.task('serve', () => {
+gulp.task('html', () => {
   gulp.src(sources.html).pipe(gulp.dest(config.dist));
 });
 
@@ -33,4 +42,14 @@ gulp.task('sass', function () {
     gulp.src(sources.rootSass)
         .pipe(sass().on("error", sass.logError)) //sass({outputStyle: "compressed"})
         .pipe(gulp.dest(config.dist + paths.assets + "css"));
+});
+
+gulp.task('js', function () {
+    // return gulp.src([sources.jquery, sources.materialize, sources.assets + "js/utils/*.js", sources.assets + "js/components/*.js", sources.rootJS])
+    return gulp.src(sources.rootJS)
+        // .pipe(concat('app.js'))
+        .pipe(browserify())
+        .pipe(rename("bundle.js"))
+        .pipe(addsrc(sources.jquery))
+        .pipe(gulp.dest(config.dist + paths.assets + "js"))
 });
