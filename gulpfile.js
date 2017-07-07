43fae4a2c58ee75bd920e88956bfdbfe08e0c18f
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-07-07 12:15:00 -0500

    Gulp-Tarea para sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-// const sass = require('gulp-sass');
+const sass = require('gulp-sass');
 // const concat = require('gulp-concat');
 
 const config = {
@@ -8,20 +8,29 @@ const config = {
 };
 
 const paths = {
-  html    : "**/*.html"
-  // assets  : "assets/",
+  html    : "**/*.html",
+  assets  : "assets/",
   // js      : "js/**/*.js",
-  // sass    : "scss/**/*.scss",
+  sass    : "scss/**/*.scss",
+  mainSass: "scss/main.scss"
   // jquery  : "js/vendor/jquery-3.2.1.min.js",
   // jsMaterialize  : "js/vendor/materialize.min.js",
-  // mainSass: "scss/main.scss",
   // mainJS  : "js/app.js"
 };
 
 const sources = {
-  html: config.source + paths.html
+  html  : config.source + paths.html,
+  assets: config.source + paths.assets,
+  sass  : config.source + paths.assets + paths.sass,
+  rootSass: config.source + paths.assets + paths.mainSass
 };
 
 gulp.task('serve', () => {
   gulp.src(sources.html).pipe(gulp.dest(config.dist));
+});
+
+gulp.task('sass', function () {
+    gulp.src(sources.rootSass)
+        .pipe(sass().on("error", sass.logError)) //sass({outputStyle: "compressed"})
+        .pipe(gulp.dest(config.dist + paths.assets + "css"));
 });
