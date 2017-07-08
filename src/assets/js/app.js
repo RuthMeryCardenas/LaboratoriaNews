06a5092084498dea1776b590d2640eda4e40a3c8
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-07-07 21:52:20 -0500

    Contenido-creación de componente

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,41 @@
+const state = {
+  mainNews : null
+}
+
+const render = (root) => {
+  root.empty();
+  const wrapper = $('<div class="wrapper"></div>');
+  wrapper.append(Content());
+  // wrapper.append(Header());
+  root.append(wrapper);
+}
+
 $( _ => {
   console.log("cargando app");
-  // const state = {
-  //   categories: null
-  // }
-  //
-  // $.get('/api/categories/', function (response){
-  //   state.categories = response;
-  //   console.log(state.categories);
-  // });
-
+  $.get('/api/categories/', function (response){
+    state.mainNews = response;
+    console.log(state.mainNews);
+    const root = $('.root');
+    render(root);
+  });
 });
+const Content = () => {
+  const content = $('<div class="content"></div>');
+  const mainNews = $('<div class="row content__mainNews"></div>');
+  const col = $('<div class="column-xs-12"></div>');
+  const newsMain = $('<div class="news-main"></div>');
+  const image = $('<div class="news-main__image"></div>');
+
+  const secondaryNews = $('<div class="row secondaryNews"></div>');
+
+
+  image.append('<img src="assets/img/news/news-0.png" alt="">');
+  newsMain.append(image);
+  col.append(newsMain);
+  mainNews.append(col);
+
+  content.append(mainNews);
+  content.append(secondaryNews);
+
+  return content;
+}
---
+++ src/assets/js/components/content.js
@@ -0,0 +1,20 @@
+// const Content = () => {
+//   const content = $('<div class="content"></div>');
+//   const mainNews = $('<div class="row content__mainNews"></div>');
+//   const col = $('<div class="column-xs-12"></div>');
+//   const newsMain = $('<div class="news-main"></div>');
+//   const image = $('<div class="news-main__image"></div>');
+//
+//   const secondaryNews = $('div class="row secondaryNews"></div>');
+//
+//
+//   image.append('<img src="assets/img/news/news-0.png" alt="">');
+//   newsMain.append(image);
+//   col.append(newsMain);
+//   mainNews.append(col);
+//
+//   content.append(mainNews);
+//   content.append(secondaryNews);
+//
+//   return Content;
+// }
